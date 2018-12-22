519bbc2c95776351d1a2e963bc7a431f449438d1
Patrick Smith <patrick@burntcaramel.com>
2018-12-23 01:32:04 +1100

    Add Cloudflare deploy terraform

---
+++ terraform/cloudflare.tf
@@ -0,0 +1,28 @@
+variable "cloudflare_email" {}
+variable "cloudflare_token" {}
+
+provider "cloudflare" {
+  email = "${var.cloudflare_email}"
+  token = "${var.cloudflare_token}"
+}
+
+resource "cloudflare_worker_script" "pipeline" {
+  zone = "collected.systems"
+  content = "${file("../index.js")}"
+}
+
+resource "cloudflare_worker_route" "collected-systems-pipeline" {
+  zone = "collected.systems"
+  pattern = "collected.systems/pipeline/*"
+  enabled = true
+
+  depends_on = ["cloudflare_worker_script.pipeline"]
+}
+
+resource "cloudflare_worker_route" "www-collected-systems-pipeline" {
+  zone = "collected.systems"
+  pattern = "www.collected.systems/pipeline/*"
+  enabled = true
+
+  depends_on = ["cloudflare_worker_script.pipeline"]
+}
