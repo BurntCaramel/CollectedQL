04465de7453044cedbecf120dc003d2fe8d7a548
Patrick Smith <patrick@burntcaramel.com>
2019-01-21 18:29:28 +1100

    Add viewport meta tag to HTML template

--- src/modules/HTML.ts
+++ src/modules/HTML.ts
@@ -4,6 +4,7 @@ const wrapHTML = (innerHTML: string) => `<!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
 * {
   box-sizing: border-box;
