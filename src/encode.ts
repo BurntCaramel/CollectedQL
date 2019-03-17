970709c610a134914adab4fddb59dfdc19fb320d
Patrick Smith <patrick@burntcaramel.com>
2019-03-17 20:39:19 +1100

    Move hex encoding to its own file

---
+++ src/encode.ts
@@ -0,0 +1,16 @@
+export function dataToHex(view: DataView): string {
+  var hexCodes = [];
+  for (var i = 0; i < view.byteLength; i += 4) {
+    // Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
+    var value = view.getUint32(i)
+    // toString(16) will give the hex representation of the number without padding
+    var stringValue = value.toString(16)
+    // We use concatenation and slice for padding
+    var padding = '00000000'
+    var paddedValue = (padding + stringValue).slice(-padding.length)
+    hexCodes.push(paddedValue);
+  }
+
+  // Join all the hex strings into one
+  return hexCodes.join("");
+}
--- src/modules/Digest.ts
+++ src/modules/Digest.ts
@@ -1,21 +1,5 @@
 import { valueToUint8Array } from "./values";
-
-function hex(view: DataView): string {
-  var hexCodes = [];
-  for (var i = 0; i < view.byteLength; i += 4) {
-    // Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
-    var value = view.getUint32(i)
-    // toString(16) will give the hex representation of the number without padding
-    var stringValue = value.toString(16)
-    // We use concatenation and slice for padding
-    var padding = '00000000'
-    var paddedValue = (padding + stringValue).slice(-padding.length)
-    hexCodes.push(paddedValue);
-  }
-
-  // Join all the hex strings into one
-  return hexCodes.join("");
-}
+import { dataToHex } from "../encode";
 
 export async function sha256(value: string | Uint8Array | ReadableStream | null): Promise<string> {
   if (!value) {
@@ -23,5 +7,5 @@ export async function sha256(value: string | Uint8Array | ReadableStream | null)
   }
 
   const data: Uint8Array = await valueToUint8Array(value);
-  return hex(new DataView(await crypto.subtle.digest("SHA-256", data)))
+  return dataToHex(new DataView(await crypto.subtle.digest("SHA-256", data)))
 }
